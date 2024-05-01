c4942ba20ee7e598c42830a80c261f3b00d29e94
theolodocoder <theolodocoder@gmail.com>
2024-05-01 22:23:53 +0100

    feat:added pages layout and 404

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { Button } from "@/components/ui/button";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Explore from "./pages/Explore";
+import Page404 from "./pages/Page404";
 
 const App: React.FC = () => {
   return (
     <div className="container mx-auto">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button variant="outline">Click from shadcn</Button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/*" element={<Page404 />} />
+      </Routes>
     </div>
   );
 };
---
+++ src/components/logo.tsx
@@ -0,0 +1,11 @@
+import LogoImg from "../assets/logo_test.svg";
+
+const Logo: React.FC = () => {
+  return (
+    <>
+      <img src={LogoImg} alt="spotlight logo" />
+    </>
+  );
+};
+
+export default Logo;
