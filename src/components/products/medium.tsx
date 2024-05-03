d0478bcc671e0194ef2ddf36dc69de4269eccc0c
theolodocoder <theolodocoder@gmail.com>
2024-05-03 01:22:48 +0100

    style:change medium border color

--- src/components/products/medium.tsx
+++ src/components/products/medium.tsx
@@ -3,7 +3,7 @@ import { IShareMedium } from "@/constants/data-constants";
 const Medium = ({ medium }: { medium: IShareMedium }) => {
   return (
     <div className="flex flex-col items-center gap-y-1 cursor-pointer">
-      <div className="w-10 h-10 border border-gray-700 grid place-items-center rounded-full">
+      <div className="w-10 h-10 border border-gray-300 grid place-items-center rounded-full">
         <img src={medium.icon} alt={medium.name} />
       </div>
       <p className="text-xs">{medium.name}</p>
