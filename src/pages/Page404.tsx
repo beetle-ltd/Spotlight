cc5078243729f37670ce09169735281b7ed522a3
theolodocoder <theolodocoder@gmail.com>
2024-05-27 00:10:23 +0100

    chore:removed hash router

--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 function Page404() {
   return (
-    <div className="flex flex-col justify-center items-center h-dvh text-center gap-y-10">
+    <div className="px-2 flex flex-col justify-center items-center h-dvh text-center gap-y-10">
       <h1 className="text-5xl font-bold m-0">Page 404</h1>
-      <p className="text-lg">
+      <p className="text-md sm:text-lg">
         You seem to be lost click the link below to go back to the home page
       </p>
       <Button size={"lg"}>
