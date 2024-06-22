62c5ef452fd5e144c39a179f11502835636d939e
theolodocoder <theolodocoder@gmail.com>
2024-06-22 19:37:06 +0100

    responsiveness added babyyyy

--- src/components/blocks/footer.tsx
+++ src/components/blocks/footer.tsx
@@ -1,28 +1,34 @@
+/* eslint-disable no-irregular-whitespace */
 import { Container } from "@/App";
 import SpotlightLogoWhite from "../../assets/spotlight-white.svg";
 import { FaInstagram, FaLinkedin } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 const Footer = () => {
   return (
-    <div className="py-5 bg-black text-white">
+    <footer className="py-5 bg-black text-white">
       <Container>
-        <div className="flex justify-between items-center">
-          <div className="flex items-center gap-x-3">
+        <div className="flex flex-col md:flex-row justify-between items-center gap-y-4 md:gap-y-0">
+          <div className="flex items-center gap-x-3 order-1 md:order-1">
             <span className="text-xs">Powered By</span>
             <img
               src={SpotlightLogoWhite}
               alt="white variant of the spotlight logo"
-              className="mb-1"
+              className="mb-1 h-5 md:h-6" // Adjust height as needed
             />
           </div>
-          <div className="flex items-center gap-x-3">
-            <FaInstagram />
-            <FaLinkedin />
-            <FaXTwitter />
+
+          <div className="flex items-center gap-x-3 order-2 md:order-3">
+            <FaInstagram className="text-lg md:text-xl hover:text-gray-300 cursor-pointer" />
+            <FaLinkedin className="text-lg md:text-xl hover:text-gray-300 cursor-pointer" />
+            <FaXTwitter className="text-lg md:text-xl hover:text-gray-300 cursor-pointer" />
+          </div>
+
+          <div className="text-xs md:text-sm text-center order-3 md:order-2">
+            Copyright © 2024 Beetle Ltd. All rights reserved.
           </div>
         </div>
       </Container>
-    </div>
+    </footer>
   );
 };
 
