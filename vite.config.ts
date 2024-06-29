072782c64020874404123ba00237e888b6869229
Daniel Brai <danielbrai.dev@gmail.com>
2024-06-30 00:00:46 +0100

    Updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,23 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      find: "./runtimeConfig",
-      replacement: "./runtimeConfig.browser",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      'process.env': env
     },
-  },
-  build: {
-    outDir: "build",
-  },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        // find: "./runtimeConfig",
+        // replacement: "./runtimeConfig.browser",
+      },
+    },
+    build: {
+      outDir: "build",
+    },
+  }
 });
