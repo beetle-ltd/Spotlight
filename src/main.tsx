e6be80a28e090871d404d889e6cf6cba5d1f57a6
theolodocoder <theolodocoder@gmail.com>
2024-06-29 22:51:47 +0100

    meregd

--- src/main.tsx
+++ src/main.tsx
@@ -1,54 +1,31 @@
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
+import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter } from "react-router-dom";
-import { render } from "react-snap";
 import App from "./App.tsx";
 import { Toaster } from "./components/ui/toaster.tsx";
 import { TooltipProvider } from "./components/ui/tooltip.tsx";
 import { SearchProvider } from "./contexts/search-context.tsx";
 import "./index.css";
 import { queryClient } from "./lib/queryClient.ts";
-import ReactDOM from "react-dom/client";
+import "./normalize.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root")!);
-
-// Check if we're on the server-side (SSR) or during pre-rendering
-if (typeof window === "undefined") {
-  // Server-side rendering (SSR) or pre-rendering
-  render(
-    <React.StrictMode>
-      <HelmetProvider>
-        <BrowserRouter>
-          <QueryClientProvider client={queryClient}>
-            <TooltipProvider>
-              <SearchProvider>
-                <Toaster />
-                <App />
-              </SearchProvider>
-            </TooltipProvider>
-          </QueryClientProvider>
-        </BrowserRouter>
-      </HelmetProvider>
-    </React.StrictMode>,
-    root
-  );
-} else {
-  // Client-side hydration
-  root.hydrate(
-    <React.StrictMode>
-      <HelmetProvider>
-        <BrowserRouter>
-          <QueryClientProvider client={queryClient}>
-            <TooltipProvider>
-              <SearchProvider>
-                <Toaster />
-                <App />
-              </SearchProvider>
-            </TooltipProvider>
-          </QueryClientProvider>
-        </BrowserRouter>
-      </HelmetProvider>
-    </React.StrictMode>
-  );
-}
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <TooltipProvider>
+            <SearchProvider>
+              <Toaster />
+              <App />
+            </SearchProvider>
+          </TooltipProvider>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </BrowserRouter>
+    </HelmetProvider>
+  </React.StrictMode>
+);
